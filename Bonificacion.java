d0ef57ccbd3fc40924f86adcfe160cc6ef92f8e8
Samuel Campo <sdcampor@gmail.com>
2026-02-27 09:28:22 -0500

    sexto commit

--- Bonificacion.java
+++ Bonificacion.java
@@ -1,59 +1,63 @@
 public class Bonificacion {
-       public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        //Declaración e inicialización
+        // Declaración e inicialización
 
-     int a = 10;
-     int b = 5;
-     int c = -3;
-     int d = 20;
+        int a = 10;
+        int b = 5;
+        int c = -3;
+        int d = 20;
 
-     double e = 2.5;
-     double f = -1.5;
-     double g = 4.8;
-     double h = 0.5;
+        double e = 2.5;
+        double f = -1.5; 
+        double g = 4.8;
+        double h = 0.5;
 
-     float i = 3.2f;
-     float j = -7.4f;
+        float i = 3.2f;
+        float j = -7.4f;
 
-     long k = 1000;
-     long l = 50000;
+        long k = 1000;
+        long l = 50000;  
+        short m = 2;
+        byte n = 1;
 
-     short m = 2;
-     byte n = 1;
+        boolean q = true;
+        boolean r = false;
 
-     boolean q = true;
-     boolean r = false;
+        double s = 2.80;
+        long t = 100000;
 
-     double s = 2.80;
-     long t = 100000;
+        // Reasignaciones
 
-        //Reansignaciones
+        a = b + c;
+        e = a * 2;
+        g = e + (e * f);
+        k = a + l;
 
-     a = b + c;
-     e = a * 2;
-     g = e + f;
-     k = a + l;
-     d = a + b;
-         //HardCode
-      b = 15;
-     c = 25;
-     f = 3.15;
-     q = false;
-     d = 11000;
-     s = 2.1416;
-     h = 0.5;
-     i = 3.3f;
-     j = -7.4f;
-     l = 5000;
-     m = 2;
-     n = 1;
-     q = true;
-     r = false;
+        d = a + b;
 
-     System.out.println("a" +a);
-     System.out.println("e" +e);
-     System.out.println("g" +g);
-     System.out.println("k" +k);
+        // HardCode
+        b = 15;
+        c = 25;
+        f = 3.15;
+        q = false;
+        d = 11000;
+        s = 2.1416;
+        h = 0.5;
+        i = 3.3f;
+        j = -7.4f;
+        l = 5000;
+        m = 2;
+        n = 1;
+        q = true;
+        r = false;
+
+        g = g + (g * f);  
+        k = k + l;        
+
+        System.out.println("a: " + a);
+        System.out.println("e: " + e);
+        System.out.println("g: " + g);
+        System.out.println("k: " + k);
     }
 }
